e3c2b90616028e6687d917c1158af401d83ae133
Nikhil <nk4338996@gmail.com>
2023-08-20 00:59:04 +0530

    Changed the logic of question answers

--- script.js
+++ script.js
@@ -22,20 +22,20 @@ const quizData = [
     correct: "b",
   },
   {
-    question: "Your thoughts on aesthetics?",
-    a: "That’s what I want. Other people have them. How do I get them?",
-    b: "They are OK, but not the only goal",
-    c: "I don't really care about aesthetics. I'm more interested in the health benefits of fitness",
-    d: "I don’t know anything about them",
-    correct: "a",
-  },
-  {
     question: "Your biggest fitness struggle right now is:",
     a: "I’m too busy",
     b: "I have no idea what exercise to do, what to eat",
     c: "Staying motivated and consistent",
     d: "I need support and accountability",
     correct: "b",
+  },
+  {
+    question: "Your thoughts on aesthetics?",
+    a: "That’s what I want. Other people have them. How do I get them?",
+    b: "They are OK, but not the only goal",
+    c: "I don't really care about aesthetics. I'm more interested in the health benefits of fitness",
+    d: "I don’t know anything about them",
+    correct: "a",
   },
   {
     question: "How motivated are you to reach your fitness goals?",
@@ -83,7 +83,7 @@ function showResult() {
   let quote = "";
 
   if (userGender === "female") {
-      const userBodyType = userAnswers[1];
+      const userBodyType = userAnswers[2];
 
       if (userBodyType === "a") {
           quote = "Lose fat and achieve a lean physique";
@@ -93,37 +93,32 @@ function showResult() {
           quote = "Build Muscles or Lose fat and achieve a lean physique";
       }
   } else if (userGender === "male") {
-      const userBodyType = userAnswers[1];
-      const userFitnessStruggle = userAnswers[3];
+      const q1 = userAnswers[2];
+      const q3 = userAnswers[4];
 
-      if (userBodyType === "a" && userFitnessStruggle === "a") {
+      if (q1 === "a") {
           quote = "Build Muscles";
-      } else if (userBodyType === "a" && userFitnessStruggle === "b") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "b" && userFitnessStruggle === "a") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "b" && userFitnessStruggle === "b") {
-          quote = "Build Muscles";
-      } else if (userBodyType === "b" && userFitnessStruggle === "c") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "b" && userFitnessStruggle === "d") {
-          quote = "Build Muscles";
-      } else if (userBodyType === "c" && userFitnessStruggle === "a") {
-          quote = "Build Muscles";
-      } else if (userBodyType === "c" && userFitnessStruggle === "b") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "c" && userFitnessStruggle === "c") {
-          quote = "Build Muscles";
-      } else if (userBodyType === "c" && userFitnessStruggle === "d") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "d" && userFitnessStruggle === "a") {
-          quote = "Build Muscles";
-      } else if (userBodyType === "d" && userFitnessStruggle === "b") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "d" && userFitnessStruggle === "c") {
-          quote = "Lose fat and achieve lean Physique";
-      } else if (userBodyType === "d" && userFitnessStruggle === "d") {
-          quote = "Build Muscles";
+      }
+      else if (q1 === "b" && q3 === "a" ) {
+        quote = "Build Muscles";
+      }
+      else if (q1 === "b" && q3 === "c") {
+        quote = "Build Muscles";
+      }
+      else if (q1 ==- "c" && q3 === "b") {
+        quote = "Lose fat and achieve Lean Physique";
+      }
+      else if (q1 === "b" && q3 === "d") {
+        quote = "Build Muscles";
+      }
+      else if (q1 === "d" && q3 === "b") {
+        quote = "Lose fat and achieve Lean Physique";
+      }
+      else if (q1 === "c" && q3 === "a") {
+        quote = "Build Muscles";
+      }
+      else if (q1 === "d" && q3 === "a") {
+        quote = "Build Muscles";
       }
   }
 
